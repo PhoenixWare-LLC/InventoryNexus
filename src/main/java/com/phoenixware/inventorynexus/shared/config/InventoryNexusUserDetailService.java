0a6743cefb903b2cc85da6ff2b35bd647e26dc35
cshort2112 <collin@webment.net>
2026-02-11 18:20:13 -0700

    feat(auth): update user authentication tables
    update user authentication tables within database schema to utilize RBAC

---
+++ src/main/java/com/phoenixware/inventorynexus/shared/config/InventoryNexusUserDetailService.java
@@ -0,0 +1,17 @@
+package com.phoenixware.inventorynexus.shared.config;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class InventoryNexusUserDetailService implements UserDetailsService {
+    /**
+     * @param username the username identifying the user whose data is required.
+     * @return
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
