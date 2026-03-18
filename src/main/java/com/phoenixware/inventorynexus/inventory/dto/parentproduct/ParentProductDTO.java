f8f68fa6f666786afe4a7fd4719561481d2acbe7
cshort2112 <collin@webment.net>
2026-03-17 22:01:15 -0600

    feat(schema): setup schema for parent prod
    setup the basic fields needed for the parent product

--- src/main/java/com/phoenixware/inventorynexus/inventory/dto/parentproduct/ParentProductDTO.java
+++ src/main/java/com/phoenixware/inventorynexus/inventory/dto/parentproduct/ParentProductDTO.java
@@ -1,5 +1,11 @@
 package com.phoenixware.inventorynexus.inventory.dto.parentproduct;
 
+import com.phoenixware.inventorynexus.shared.entity.Asset;
+import com.phoenixware.inventorynexus.shared.validation.Create;
+import com.phoenixware.inventorynexus.shared.validation.Get;
+import com.phoenixware.inventorynexus.shared.validation.Patch;
+import com.phoenixware.inventorynexus.shared.validation.Update;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,5 +23,58 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ParentProductDTO {
+    @Null(groups = Create.class)
     private UUID id;
+
+    @Null(groups = {
+            Create.class,
+            Update.class,
+            Patch.class
+    })
+    private Asset asset;
+
+    @NotBlank(
+            groups = {
+                    Get.class,
+                    Create.class,
+                    Update.class
+            },
+            message = "title cannot be blank!"
+    )
+    @Size(min = 1, max = 50, message = "title must be between 1 and 50 characters!")
+    private String title;
+
+    @NotBlank(
+            groups = {
+                    Get.class,
+                    Create.class,
+                    Update.class
+            },
+            message = "title cannot be blank!"
+    )
+    @Size(min = 1, max = 255, message = "title must be between 1 and 255 characters!")
+    private String description;
+
+    @NotBlank(
+            groups = {
+                    Get.class,
+                    Create.class,
+                    Update.class
+            },
+            message = "title cannot be blank!"
+    )
+    @Size(min = 1, max = 500, message = "title must be between 1 and 500 characters!")
+    private String notes;
+
+    @NotNull(
+            groups = {
+                    Get.class,
+                    Create.class,
+                    Update.class
+            },
+            message = "title cannot be null!"
+    )
+    @Min(value = 1)
+    @Max(value = 100)
+    private Integer targetMargin;
 }
